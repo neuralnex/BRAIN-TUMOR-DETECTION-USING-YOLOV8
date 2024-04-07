ec85441396e5671a03ad471c5a1c11fe2501c013
Omeziri Zion <108340058+InvestorZION@users.noreply.github.com>
2024-04-07 09:30:27 +0100

    Add files via upload

---
+++ Brain_tumor_detection.py
@@ -0,0 +1,15 @@
+from roboflow import Roboflow
+rf = Roboflow(api_key="m38E5eAth8zRPnpu69az")
+project = rf.workspace().project("brain-tumor-detection-u7ysj")
+model = project.version(1).model
+
+# infer on a local image
+print(model.predict("b.jpg", confidence=40, overlap=30).json())
+
+# visualize your prediction
+model.predict("b.jpg", confidence=40, overlap=30).save("JK.jpg")
+
+# infer on an image hosted elsewhere
+# print(model.predict("URL_OF_YOUR_IMAGE", hosted=True, confidence=40, overlap=30).json())
+import torch
+torch.save(model, "model.pt")
